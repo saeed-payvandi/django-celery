99040f8f0caa3b10c57c56b6b60460e57560e4c7
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-11-26 18:52:47 +0330

    Add celery command to docker-compose, communicate django and celery by config __init__.py, Create a simple task for the first celery task

--- config/celery.py
+++ config/celery.py
@@ -3,4 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 app = Celery('config')
-app.conf.from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
