9089907c5eb7c264d62107c7eecee21874a13537
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-12-11 19:28:32 +0330

    Create some tasks for checking priority

--- eshop/notifications/tasks.py
+++ eshop/notifications/tasks.py
@@ -1,10 +1,22 @@
 from celery import shared_task
-
-
-@shared_task
-def send_discount_emails():
-    pass
+import time
 
 @shared_task
-def process_data_for_ml():
-    pass
+def task_1(queue='celery'):
+    time.sleep(3)
+    return
+
+@shared_task
+def task_2(queue='celery:1'):
+    time.sleep(3)
+    return
+
+@shared_task
+def task_3(queue='celery:2'):
+    time.sleep(3)
+    return
+
+@shared_task
+def task_4(queue='celery:3'):
+    time.sleep(3)
+    return
