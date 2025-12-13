8fd55db4ddc521415b0160a13f333be291600793
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-12-13 15:03:43 +0330

    Install pika package and config docker-compose and celery.py for use RabbitMQ

--- eshop/config/celery.py
+++ eshop/config/celery.py
@@ -1,5 +1,7 @@
 import os
 from celery import Celery
+from kombu import Exchange, Queue
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 app = Celery('config')
@@ -14,10 +16,20 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # queue=>celery,celery:1,celery:2,celery:3
 
-app.conf.broker_transport_options = {
-    'priority_steps': list(range(10)),
-    'sep': ':',
-    'queue_order_strategy': 'priority',
-}
+# app.conf.broker_transport_options = {
+#     'priority_steps': list(range(10)),
+#     'sep': ':',
+#     'queue_order_strategy': 'priority',
+# }
+
+app.conf.task_queues = [
+    Queue('tasks', Exchange('tasks'), routing_key='tasks',
+          queue_arguments={'x-max-priority': 10}),
+]
+
+app.conf.task_acks_late = True
+app.conf.task_default_priority = 5
+app.conf.worker_prefetch_multiplier = 1
+app.conf.worker_concurrency = 1
 
 app.autodiscover_tasks()
