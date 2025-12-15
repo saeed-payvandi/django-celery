7dd7630a6ef0c93d9d6bd0f20d93173271df0496
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-12-15 21:05:56 +0330

    Create send_sms_to_user function for test retry a task

--- eshop/notifications/tasks/sms.py
+++ eshop/notifications/tasks/sms.py
@@ -1,5 +1,11 @@
 from celery import shared_task
+from config.celery_config import app
 
-@shared_task(queue='tasks')
+@app.task(queue='tasks', autoretry_for=(ConnectionError,), default_retry_delay=5, retry_kwargs={'max_retries': 5})
 def send_sms_to_user():
-    print('sms has been sent to user')
+    raise ConnectionError('sms connection error....')
+
+
+# @shared_task(queue='tasks')
+# def send_sms_to_user():
+#     print('sms has been sent to user')
