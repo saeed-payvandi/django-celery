07fc0656ce206a58e93b54455ea90fb9a8e5b21a
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-12-18 20:18:43 +0330

    Create send_email function for testing how timeout works

--- eshop/notifications/tasks/email.py
+++ eshop/notifications/tasks/email.py
@@ -3,10 +3,20 @@ from config.celery_config import app
 import time
 
 
-@app.task(queue='tasks', time_limit=5)
+@app.task(queue='tasks', time_limit=5) # after time_limit if task doesn't finish, the task will destroy
 def send_mail_to_user():
     time.sleep(6)
     return "(* Email has been set to user successfully *)"
+
+
+def send_email():
+    result = send_mail_to_user.delay()
+
+    try:
+        task_result = result.get(timeout=4) # with timeout we will have a timeout message but the task will continue
+    except TimeoutError:
+        print("Task timed out")
+        # result.revoke(terminate=True)
 
 
 # @shared_task(queue='tasks')
