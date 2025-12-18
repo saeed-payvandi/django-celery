a9e7b4a8eaae266f999d5d8712fbe9ecbde5eaa7
saeed-payvandi <saeedpayvandi82@gmail.com>
2025-12-18 19:43:37 +0330

    Add time_limit to send_mail_to_user function

--- eshop/notifications/tasks/email.py
+++ eshop/notifications/tasks/email.py
@@ -1,6 +1,15 @@
 from celery import shared_task
+from config.celery_config import app
+import time
 
-@shared_task(queue='tasks')
+
+@app.task(queue='tasks', time_limit=5)
 def send_mail_to_user():
-    # print('email has been send to user')
-    raise ConnectionError('connection lost...')
+    time.sleep(6)
+    return "(* Email has been set to user successfully *)"
+
+
+# @shared_task(queue='tasks')
+# def send_mail_to_user():
+#     # print('email has been send to user')
+#     raise ConnectionError('connection lost...')
